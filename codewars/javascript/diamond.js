183361f6ac8ac27dcee10ed31e42f30fdb75d081
Greg RV <gregvarias@gmail.com>
2015-12-16 00:07:30 -0800

    Diamond complete.

--- codewars/javascript/diamond.js
+++ codewars/javascript/diamond.js
@@ -1,32 +1,30 @@
 function diamond(n){
+  // for each line, print N-current whitespace
+  // divide that whitespace by 2, and place *'s in between
+
+  // edge cases
   if (n % 2 === 0 || n < 3) return null;
 
-  // the middle of the diamon has N *'s with no whitespace
-  // each line above and below has N = N-2 *'s until N = 1
-
-  // for each line, print N-current whitespace
-    // divide that whitespace by 2, and place *'s in between
-
-  // build up
   var rowStarCount = 1;
   var rowWhiteSpace = 0;
   var diamond = '';
 
+  // build top half of diamond
   while (rowStarCount <= n) {
     rowWhiteSpace = (n - rowStarCount) / 2;
-    diamond += repeatString(' ', rowWhiteSpace) + repeatString('*', rowStarCount) + repeatString(' ', rowWhiteSpace) + '\n';
+    diamond += repeatString(' ', rowWhiteSpace) + repeatString('*', rowStarCount) + '\n';
     rowStarCount += 2;
   }
 
-  // build down
+  // build bottom half of diamond
+  rowStarCount = n - 2;
+  while (rowStarCount >= 1) {
+    rowWhiteSpace = (n - rowStarCount) / 2;
+    diamond += repeatString(' ', rowWhiteSpace) + repeatString('*', rowStarCount) + '\n';
+    rowStarCount -= 2;
+  }
 
-  //   *
-  //  ***
-  // *****
-  //  ***
-  //   *
-
-  console.log(diamond);
+  return diamond;
 }
 
 function repeatString(str, n) {
@@ -38,8 +36,7 @@ function repeatString(str, n) {
 }
 
 // TESTS
-// console.log(diamond(3) === " *\n***\n *\n");
-diamond(3);
-// console.log(diamond(2) === null);
-// console.log(diamond(-3) === null);
-// console.log(diamond(0) === null);
+console.log(diamond(3) === " *\n***\n *\n");
+console.log(diamond(2) === null);
+console.log(diamond(-3) === null);
+console.log(diamond(0) === null);
